a53eb3e5d5bcc21bba5acbf31a5cba089e530433
DESKTOP-T68I1OQ\Vithor <imaginebrealer@gmail.com>
2020-11-25 17:16:42 -0300

    filter + dark theme

--- app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
+import com.example.android.gdgfinder.R
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -31,7 +33,7 @@ class GdgListFragment : Fragment() {
         val binding = FragmentGdgListBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
@@ -56,6 +58,35 @@ class GdgListFragment : Fragment() {
                 }
             }
         })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+
+                //make a new Chip view for each item in the list
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children: List<Chip> = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+
+                //remove any views already in the ChipGroup
+                chipGroup.removeAllViews()
+
+                //add the new children to the ChipGroup
+                for(chip in children) {
+                    chipGroup.addView(chip)
+                }
+            }
+        })
+
 
         setHasOptionsMenu(true)
         return binding.root
@@ -134,5 +165,6 @@ class GdgListFragment : Fragment() {
         }
     }
 }
+
 
 
